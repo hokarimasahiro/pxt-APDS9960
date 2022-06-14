39736c0ff6725f67be7d2083bd86565ff47a0b2d
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2022-06-14 15:48:39 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,11 @@
-apds9960.start(2);
+if (apds9960.start(2)) {
+    basic.showIcon(IconNames.Heart)
+} else {
+    basic.showIcon(IconNames.Confused)
+}
 serial.redirectToUSB()
 basic.forever(function() {
     let als =apds9960.getAls();
     serial.writeNumbers(als);
+    basic.pause(1000)
 })
